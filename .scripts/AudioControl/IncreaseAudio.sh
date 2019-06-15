98ba9ca1985ef86d290180a2eb54b7c01134c658
BigB00st <botzer.2002@gmail.com>
2019-06-16 01:29:48 +0300

    added scripts

---
+++ .scripts/AudioControl/IncreaseAudio.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+x=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' )
+if [[ $x -le 95 ]]
+then
+    pactl set-sink-volume 0 +5% 
+fi
