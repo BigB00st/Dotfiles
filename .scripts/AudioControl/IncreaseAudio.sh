0d2abce343d26596b65a74f90f607012c9eac0f1
BigB00st <botzer.2002@gmail.com>
2019-06-16 03:10:38 +0300

    improved IncreaseVolume.sh

--- .scripts/AudioControl/IncreaseAudio.sh
+++ .scripts/AudioControl/IncreaseAudio.sh
@@ -2,5 +2,7 @@
 x=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' )
 if [[ $x -le 95 ]]
 then
-    pactl set-sink-volume 0 +5% 
+	pactl set-sink-volume 0 +5%
+else
+	pactl set-sink-volume 0 100%
 fi
