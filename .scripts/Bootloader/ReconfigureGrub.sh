4c102d4a4961e8822e01b44f2e4e077e4d8a9b05
BigB00st <botzer.2002@gmail.com>
2019-07-17 11:09:51 +0300

    root required in GrubReconfigure

--- .scripts/Bootloader/ReconfigureGrub.sh
+++ .scripts/Bootloader/ReconfigureGrub.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 sudo mount -t ntfs-3g -o remove_hiberfile /dev/sda2 /mnt/windows
 sudo grub-mkconfig -o /boot/grub/grub.cfg 
 sudo umount /mnt/windows
