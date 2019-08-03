90fc5c0b91b1e58fb1986b7ed11e5ae157b09f43
BigB00st <botzer.2002@gmail.com>
2019-08-03 15:56:47 +0300

    updated maintenence script, edited aliases to work with ranger, set workspace for discord

--- .scripts/system/maintenance.sh
+++ .scripts/system/maintenance.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
-sudo pacman -Syu # update packages
-paccache -rk1 # keep only previous version 
+yay # update packages
+sudo paccache -rk1 /var/cache/pacman/pkg $HOME/.cache/yay # keep only previous version 
