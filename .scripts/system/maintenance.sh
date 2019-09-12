32f8ba44efa65394729101869528892a7914f2a8
BigB00st <botzer.2002@gmail.com>
2019-09-12 10:10:31 +0300

    recursive solution for cleaning yay cache

--- .scripts/system/maintenance.sh
+++ .scripts/system/maintenance.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 yay # update packages
-sudo paccache -rk1
-sudo paccache -rk1 $HOME/.cache/yay # keep only previous version 
+sudo paccache -rk1 #remove pacman cache
+sudo paccache -rk1 $(find $(yay -Pg | jq -r .buildDir) -maxdepth 1 -type d | awk '{ print "-c " $1 }' | tail -n +2) #remove yay cache
